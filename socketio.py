22cb28ca436c4828099e25544b560b9f56bd16a4
Wilfred Apollon <dev@achila.me>
2025-05-04 13:07:32 -0500

    incremental product refinement

--- socketio.py
+++ socketio.py
@@ -5,6 +5,10 @@ except ImportError:
      
 import asyncio as sync
 import time as T
+import socket 
+from socket import gethostbyaddr,
+                 getservbyport,
+
 from ss import BaseRequestHandler, 
                 StreamRequestHandler, 
                 ForkingUDPServer, 
@@ -29,6 +33,31 @@ get_log = log.getLogger('com.heustric_reasoning.app %()s')
 
 def __init__(self):
    self.origins = {}
+
+
+ip6 = has_ipv6
+word = htonl
+dword htons
+oust = getdefaulttimeout
+
+set_of_expectations = (
+    #combine the possibility of socket errors here
+    # if a defaulttimeout of return occur, return it's error here 
+    "err" 
+)
+
+async def get_hostname_by_ip(link):
+    await host = gethostbyaddr(link)
+    return host
+
+async def get_host_services (porto, protocoltypeb):
+    await service = getservbyport
+    if service is err 
+    return service 
+
+async def get_localhost_netlink():
+    await localpid 
+
 
 async def link(self): 
     self 
